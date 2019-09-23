51fadd42ce4a36d7e7f8e30ca8b4b4caed93cd67
Panu Chaisrirak <liu@MacBook-Air-khxng-Panu.local>
2019-09-23 17:45:09 +0700

    inert to Firebase

--- App/insertFB.py
+++ App/insertFB.py
@@ -1,15 +1,17 @@
-def insert(label,title,des):
-    import firebase_admin
-    from firebase_admin import credentials
-    from firebase_admin import db
-    import datetime
 
-    # Fetch the service account key JSON file contents
-    cred = credentials.Certificate('bot.json')
-    # Initialize the app with a service account, granting admin privileges
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://bot-label-615f6.firebaseio.com'
-    })
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import datetime
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('bot.json')
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://bot-label-615f6.firebaseio.com'
+})
+def insert(label,title,des):
+    
     ref = db.reference('DATA')
     ref.push({
         "Date": str(datetime.datetime.now()),
