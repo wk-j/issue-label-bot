6603f451244d93021ffca4d902d5395c09fb5a86
Panu Chaisrirak <liu@MacBook-Air-khxng-Panu.local>
2019-09-23 17:08:02 +0700

    inert to Firebase

--- App/insertFB.py
+++ App/insertFB.py
@@ -1,33 +1,19 @@
 def insert(label,title,des):
-    import pyrebase
+    import firebase_admin
+    from firebase_admin import credentials
+    from firebase_admin import db
     import datetime
-    config = {
-        "apiKey": "AIzaSyAt0kO8MDeqCWOLAMaaPxDS0yF8UkVPEdo",
-        "authDomain": "bot-label-615f6.firebaseapp.com",
-        "databaseURL": "https://bot-label-615f6.firebaseio.com",
-        "projectId": "bot-label-615f6",
-        "storageBucket": "bot-label-615f6.appspot.com",
-        "messagingSenderId": "188295128754",
-        "appId": "1:188295128754:web:9666ba1caef90ab87c0bab"
-    }
-    firebase = pyrebase.initialize_app(config)
 
-    # Get a reference to the auth service
-    auth = firebase.auth()
-
-    # Log the user in
-    #auth.create_user_with_email_and_password('test@gmail.com', 'sunner00x')
-    user = auth.sign_in_with_email_and_password('test@gmail.com', 'test00x')
-
-    # Get a reference to the database service
-    db = firebase.database()
-
-    # data to save
-    data = {
+    # Fetch the service account key JSON file contents
+    cred = credentials.Certificate('bot.json')
+    # Initialize the app with a service account, granting admin privileges
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://bot-label-615f6.firebaseio.com'
+    })
+    ref = db.reference('DATA')
+    ref.push({
         "Date": str(datetime.datetime.now()),
         "Label":label,
-        "Title": title,
+        "Title":title,
         "Description":des
-    }
-    # Pass the user's idToken to the push method
-    results = db.child("DATA").push(data, user['idToken'])
+    })
--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ RUN pip3 install -r requirements.txt
 
 COPY App/bot-label.pem /app
 COPY App/model.data /app
+COPY App/bot.json /app
 COPY App/vocabulary.data /app
 COPY App/insertdata.py /app
 COPY App/DBconfig.py /app
